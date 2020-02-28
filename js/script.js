d9674c1f40905343a526ea4304dc8ed6efe583e5
sebokaro <sebokaro@gmail.com>
2020-02-28 14:08:53 +0100

    Create main page of Donostia-San Srbastian

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const nav = document.getElementById('nav');
+const navSpace = document.querySelector('.nav__space');
+const menuSpans = document.querySelectorAll('.nav__menu');
+const navIcons = document.querySelectorAll('.nav__icon');
+const headerInfoMain = document.querySelector('.header__info-main');
+const headerInfoSecond = document.querySelector('.header__info-second');
+const closingClasses = ['nav__menu--close-first', 'nav__menu--close-second', 'nav__menu--close-third'];
+const clickingClasses = ['nav__menu--click-first', 'nav__menu--click-second', 'nav__menu--click-third'];
+
+document.addEventListener('DOMContentLoaded', () => {
+  headerInfoMain.classList.add('header__info-main--show');
+  headerInfoSecond.classList.add('header__info-second--show');
+});
+
+nav.addEventListener('click', () => {
+  menuSpans.forEach((span, i) => {
+    span.classList.toggle(`${closingClasses[i]}`);
+    span.classList.toggle(`${clickingClasses[i]}`);
+  });
+  navIcons.forEach(icon => {
+    icon.classList.toggle('nav__icon--show');
+  });
+  navSpace.classList.toggle('nav__space--show');
+});
