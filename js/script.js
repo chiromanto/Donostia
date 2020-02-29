6d61fe3833e76d4265a35cc6c8d14fb0ce23c5ff
sebokaro <sebokaro@gmail.com>
2020-02-29 18:06:29 +0100

    Create a subpage attractions.html

--- js/script.js
+++ js/script.js
@@ -20,7 +20,3 @@ nav.addEventListener('click', () => {
   });
   navSpace.classList.toggle('nav__space--show');
 });
-
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
