1439a1fef46649355700799fddef7b820192d390
sebokaro <sebokaro@gmail.com>
2020-03-01 17:22:11 +0100

    Correct small things in whole project

--- js/script.js
+++ js/script.js
@@ -5,10 +5,13 @@ const navIcons = document.querySelectorAll('.nav__icon');
 const headerInfoMain = document.querySelector('.header__info-main');
 const headerInfoSecond = document.querySelector('.header__info-second');
 const closingClasses = ['nav__menu--close-first', 'nav__menu--close-second', 'nav__menu--close-third'];
+// const navLinks = document.querySelectorAll('.nav__link');
 
 document.addEventListener('DOMContentLoaded', () => {
   headerInfoMain.classList.add('header__info-main--show');
-  headerInfoSecond.classList.add('header__info-second--show');
+  if (headerInfoSecond) {
+    headerInfoSecond.classList.add('header__info-second--show');
+  }
 });
 
 nav.addEventListener('click', () => {
@@ -20,3 +23,16 @@ nav.addEventListener('click', () => {
   });
   navSpace.classList.toggle('nav__space--show');
 });
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', e => {
+//     navLinks.forEach(link => {
+//       if (link.classList.contains('nav__link--current')) {
+//         link.classList.remove('nav__link--current');
+//       }
+//     });
+//     console.log(link);
+//     // e.preventDefault();
+//     e.currentTarget.classList.add('nav__link--current');
+//   });
+// });
