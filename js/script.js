3469282e6bf29184123513a77dfb9c0453aefd98
sebokaro <sebokaro@gmail.com>
2020-03-02 17:21:25 +0100

    Add some tables to the content and change colors

--- js/script.js
+++ js/script.js
@@ -5,12 +5,18 @@ const navIcons = document.querySelectorAll('.nav__icon');
 const headerInfoMain = document.querySelector('.header__info-main');
 const headerInfoSecond = document.querySelector('.header__info-second');
 const closingClasses = ['nav__menu--close-first', 'nav__menu--close-second', 'nav__menu--close-third'];
-// const navLinks = document.querySelectorAll('.nav__link');
+const headerUnderLines = document.querySelectorAll('.content__header');
+const currentScroll = window.pageYOffset;
 
 document.addEventListener('DOMContentLoaded', () => {
   headerInfoMain.classList.add('header__info-main--show');
   if (headerInfoSecond) {
     headerInfoSecond.classList.add('header__info-second--show');
+  }
+
+  //Drawing line under first header
+  if (headerUnderLines[0]) {
+    headerUnderLines[0].classList.add('content__header--show-line');
   }
 });
 
@@ -24,15 +30,12 @@ nav.addEventListener('click', () => {
   navSpace.classList.toggle('nav__space--show');
 });
 
-// navLinks.forEach(link => {
-//   link.addEventListener('click', e => {
-//     navLinks.forEach(link => {
-//       if (link.classList.contains('nav__link--current')) {
-//         link.classList.remove('nav__link--current');
-//       }
-//     });
-//     console.log(link);
-//     // e.preventDefault();
-//     e.currentTarget.classList.add('nav__link--current');
-//   });
-// });
+//Drawing lines under other headers
+window.addEventListener('scroll', () => {
+  headerUnderLines.forEach(header => {
+    if (header.offsetTop - window.pageYOffset < 400) {
+      header.classList.add('content__header--show-line');
+      console.log('działa');
+    }
+  });
+});
